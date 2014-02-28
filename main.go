937f39badcac2c6315cc2f88afd92c2841039324
Danny van Heumen <danny@dannyvanheumen.nl>
2014-02-28 22:50:35 +0100

    Added support for multiple forms of redirect checking.

--- main.go
+++ main.go
@@ -12,14 +12,37 @@ import (
 	"sync"
 )
 
-const NUM_RESOLVERS = 3
+const (
+	NUM_RESOLVERS = 3
+)
 
-var errDone = errors.New("not following any more redirects")
+var (
+	errDone = errors.New("not following any more redirects")
+	prots   = []string{
+		"http",
+		"https",
+	}
+)
 
-var prots = []string{
-	"http",
-	"https",
-}
+type RedirectBehavior func(req *http.Request, via []*http.Request) error
+
+var (
+	// Follow all redirects, only find out about final pages.
+	FollowAllRedirects = func(req *http.Request, via []*http.Request) error {
+		return nil
+	}
+	// Stop on first redirect encountered.
+	StopOnFirstRedirect = func(req *http.Request, via []*http.Request) error {
+		return errDone
+	}
+	// Stop as soon as you are redirected to a different domain.
+	StopOnRedirectToDifferentDomain = func(req *http.Request, via []*http.Request) error {
+		if req.URL.Host != via[len(via)-1].URL.Host {
+			return errDone
+		}
+		return nil
+	}
+)
 
 type Site struct {
 	RequestUrl  url.URL
@@ -35,9 +58,6 @@ func (s *Site) String() string {
 	return line
 }
 
-// TODO detect immediate redirect
-// TODO follow redirects, detect moving off domain
-// TODO follow all redirects, detect final pages such as errors
 func main() {
 	// start result writer
 	var writerGroup sync.WaitGroup
@@ -48,10 +68,11 @@ func main() {
 	var work = make(chan string)
 	go readWorker("urls.txt", work)
 	// start checkers
+	var checkRedirect = FollowAllRedirects
 	var workerGroup sync.WaitGroup
 	for i := 0; i < NUM_RESOLVERS; i++ {
 		workerGroup.Add(1)
-		go checkWorker(&workerGroup, work, result)
+		go checkWorker(&workerGroup, work, result, checkRedirect)
 	}
 	// wait for all checkers to finish
 	workerGroup.Wait()
@@ -76,21 +97,19 @@ func readWorker(filepath string, work chan<- string) {
 }
 
 func openUrls(filepath string) io.Reader {
-	return strings.NewReader("google.com\nwww.google.com\ntweakers.net\nwww.tweakers.net\nsecurity.nl\nwww.security.nl")
+	return strings.NewReader("google.com\nwww.google.com\ntweakers.net\nwww.tweakers.net\nsecurity.nl\nwww.security.nl\nwww.em-te.nl\ngmail.com\nwww.van-hoeckel.nl\nvan-hoeckel.nl")
 }
 
-func checkWorker(wg *sync.WaitGroup, work <-chan string, result chan<- *Site) {
+func checkWorker(wg *sync.WaitGroup, work <-chan string, result chan<- *Site, check RedirectBehavior) {
 	defer wg.Done()
 	var c http.Client
-	c.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		return errDone
-	}
+	c.CheckRedirect = check
 	for site := range work {
 		r, err := testUrl(&c, site)
 		if err == nil {
 			result <- r
 		} else {
-			os.Stderr.WriteString(err.Error())
+			os.Stderr.WriteString(err.Error() + "\n")
 		}
 	}
 }
